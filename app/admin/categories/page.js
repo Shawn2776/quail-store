91ee076fe054e1780e7c84c1769d81a50dfc737e
Shawn Harrington <shawn.harrington2776@gmail.com>
2026-08-20 19:22:50 -0700

    added admin categories - tsing

--- app/admin/categories/page.js
+++ app/admin/categories/page.js
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { updateCategory } from "./actions";
 
+export const dynamic = "force-dynamic";
+
 export default async function CategoriesPage() {
   const categories = await prisma.category.findMany({
     orderBy: { createdAt: "asc" },
